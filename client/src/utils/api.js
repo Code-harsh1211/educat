4461bc160d69317b6d29feeadd5ad1eee9e887ff
harsh-codes12 <hrsh.er@icloud.com>
2026-04-25 23:05:09 +0530

    fixed api base url

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://educat-c1sz.onrender.com',
+  baseURL: 'https://educat-c1sz.onrender.com/api',
   timeout: 30000,
 });
 
